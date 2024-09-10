7b9e0b52f8bb410e2c228d85548397f852ca705f
richardm6899 <richardmorrissw@gmail.com>
2024-09-10 13:40:12 +0200

    Quest Fields and constructor added

---
+++ Quests.cs
@@ -0,0 +1,13 @@
+public class Quest
+{
+    public int ID;
+    public string Name;
+    public string Description;
+
+    public Quest(int id, string name, string description)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.Description = description;
+    }
+}

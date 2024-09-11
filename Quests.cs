52c9e049ee74aca9ef3b2e706a4f2c3f6eac67f7
richardm6899 <richardmorrissw@gmail.com>
2024-09-11 13:13:36 +0200

    CancelQuest() made

--- Quests.cs
+++ Quests.cs
@@ -10,4 +10,62 @@ public class Quest
         this.Name = name;
         this.Description = description;
     }
+
+    /* There is a list of quests that need to be cleared, when starting going into a location with a quest,
+    the player will be asked to start the quest which gets the Name of the quest ID to start from the list,
+    the same would apply viceverse, if the player chooses to cancel, the quests gets the Name of the quest 
+    The player can flee from a monster, cancelling the current quest by rolling a certain number on dice(random num).
+    */
+
+    // As a player I want to fight a monster to complete a quest
+
+    public static void StartQuest(int questID, int monsterID)
+    {
+        Mission.World.QuestByID(questID);
+        if (questID == 1)
+        {
+            Console.WriteLine("Clear Blacksmith Garden quest accepted");
+            Console.WriteLine(mission.Name);
+            Console.WriteLine(mission.Description);
+
+        }
+        else if (questID == 2)
+        {
+            Console.WriteLine("Collect Blacksmith items quest accepted");
+        }
+        else if (questID == 3)
+        {
+            Console.WriteLine("Collect Alchemist items quest accepted");
+        }
+        else if (questID == 4)
+        {
+            Console.WriteLine("Save Halflings quest accepted");
+        }
+        else if (questID == 5)
+        {
+            Console.WriteLine("Kill Aragog quest accepted");
+        }
+        else
+        {
+            Console.WriteLine("No quest available");
+        }
+    }
+    public static bool CancelQuest()
+    {
+        int rolldice = new Random().Next(1, 7);
+        int fleethreshhold = 3;
+
+        if (rolldice > fleethreshhold)
+        {
+            return true; // The player flees,the quest is cancelled
+        }
+        else
+        {
+            return false; // The player suffers consequences, the quest fails to cancel
+        }
+    }
+    public static void Main()
+    {
+        StartQuest(1, 1);
+    }
 }

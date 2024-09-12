16aa7a573659376db05a82256d391bf19aeb7323
richardm6899 <richardmorrissw@gmail.com>
2024-09-12 16:49:30 +0200

    Further developed EnterLocationWithAQuest(), StartQuest() has quests/simple tasks

--- Quests.cs
+++ Quests.cs
@@ -1,8 +1,20 @@
+using System.ComponentModel.Design;
+using System.Formats.Asn1;
+using System.Reflection.PortableExecutable;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
 public class Quest
 {
     public int ID;
     public string Name;
     public string Description;
+
+    public static bool victory1 = false;
+    public static bool victory2 = false;
+    public static bool victory3 = false;
+    public static bool victory4 = false;
+    public static bool victory5 = false;
 
 
     public Quest(int id, string name, string description)
@@ -12,45 +24,227 @@ public class Quest
         this.Description = description;
     }
 
-    /* There is a list of quests that need to be cleared, when starting going into a location with a quest,
-    the player will be asked to start the quest which gets the Name of the quest ID to start from the list,
-    the same would apply viceverse, if the player chooses to cancel, the quests gets the Name of the quest 
-    The player can flee from a monster, cancelling the current quest by rolling a certain number on dice(random num).
-    */
 
-    // As a player I want to fight a monster to complete a quest
 
+
+    // Enters location with a quest, user is asked if he wants to start, once started he must finish the quest.
+
+    public static void EnterLocationWithAQuest(int LocationID)
+    {
+        Location location = World.LocationByID(LocationID);
+        if (LocationID == 3)
+        {
+            Console.WriteLine("You have entered " + location.Name + ". Would you like to start the quest? (y/n)");
+            string input = Console.ReadLine();
+            if (input == "y")
+            {
+                StartQuest(1, 1, 3);
+            }
+            else
+            {
+                Dont_StartQuest();
+            }
+        }
+        else if (LocationID == 5)
+        {
+            Console.WriteLine("You have entered " + location.Name + ". Would you like to start the quest? (y/n)");
+            string input = Console.ReadLine();
+            if (input == "y")
+            {
+                StartQuest(2, 2, 5);
+            }
+            else
+            {
+                Dont_StartQuest();
+            }
+        }
+        else if (LocationID == 7)
+        {
+            Console.WriteLine("You have entered " + location.Name + ". Would you like to start the quest? (y/n)");
+            string input = Console.ReadLine();
+            if (input == "y")
+            {
+                StartQuest(3, 3, 7);
+            }
+            else
+            {
+                Dont_StartQuest();
+            }
+        }
+        else if (LocationID == 9)
+        {
+            Console.WriteLine("You have entered " + location.Name + ". Would you like to start the quest? (y/n)");
+            string input = Console.ReadLine();
+            if (input == "y")
+            {
+                StartQuest(4, 4, 9);
+            }
+            else
+            {
+                Dont_StartQuest();
+            }
+        }
+        else if (LocationID == 11)
+        {
+            Console.WriteLine("You have entered " + location.Name + ". Would you like to start the quest? (y/n)");
+            string input = Console.ReadLine();
+            if (input == "y")
+            {
+                StartQuest(5, 5, 11);
+            }
+            else
+            {
+                Dont_StartQuest();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Wrong location ID");
+        }
+    }
+
+
+    // Notifies user he'll be returned to previous location
+    public static void Dont_StartQuest() //int questID, int monsterID, int LocationID
+    {
+        Console.WriteLine("You'll be returned where you came from. Say when you are ready to start the quest.");
+        //Implement logic to return to previous location
+    }
+
+
+    // Starts quest and needs requirements in order to proceed, quests partly implemented, needs fight methods
     public static void StartQuest(int questID, int monsterID, int LocationID)
     {
         Quest mission = World.QuestByID(questID); //Making object
-        if (questID == 1)
+        Location location = World.LocationByID(LocationID);
+        Monster monster = World.MonsterByID(monsterID);
+        if (mission.ID == 1)
         {
             Console.WriteLine("Clear Blacksmith Garden quest accepted");
             Console.WriteLine(mission.Name);
             Console.WriteLine(mission.Description);
+            if (monster.ID == 1)
+            {
+                //Introduce quest method logic
+                if (monster.CurrentHitPoints == 0)
+                {
+                    Console.WriteLine($"You have killed {monster.Name}. Return to the blacksmith for your reward and your next quest.");
+                }
+                else
+                {
+                    Console.WriteLine("Keep trying!");
+                    victory1 = true; // must be in the statement above
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect monster for this zone");
+            }
 
         }
-        else if (questID == 2)
+        else if (questID == 2 && victory1 == true)
         {
+            bool menu2 = true;
             Console.WriteLine("Collect Blacksmith items quest accepted");
+            Console.WriteLine(mission.Name);
+            Console.WriteLine(mission.Description);
+            {
+                while (menu2 == true)
+                {
+                    //Introduce quest method logic
+                    Console.WriteLine("In order to collect the tools out you must answer the following riddle correctly");
+                    Console.WriteLine("Which letter of the alphabet has the most water? (Type in the letter)");
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "c")
+                    {
+                        Console.WriteLine("Correct! You've collected the tools. You received a reward that will aid you in the alchemist, where your next quest awaits.");
+                        menu2 = false;
+                        victory2 = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect. Try again.");
+                    }
+                }
+            }
         }
-        else if (questID == 3)
+        else if (questID == 3 && victory2 == true)
         {
             Console.WriteLine("Collect Alchemist items quest accepted");
+            Console.WriteLine(mission.Name);
+            Console.WriteLine(mission.Description);
+            if (monster.ID == 3)
+            {
+                //Introduce quest method logic
+                if (monster.CurrentHitPoints == 0)
+                {
+                    Console.WriteLine($"You have killed {monster.Name}. Return to the Alchemist for your reward and your next quest awaits in the shop basement.");
+                    victory3 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Keep trying!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect monster for this zone");
+            }
         }
         else if (questID == 4)
         {
             Console.WriteLine("Save Halflings quest accepted");
+            Console.WriteLine(mission.Name);
+            Console.WriteLine(mission.Description);
+            bool menu1 = true;
+            //Introduce quest method logic
+            Console.WriteLine("In order to save the halflings out you must answer the following riddle correctly");
+            while (menu1 == true)
+            {
+                Console.WriteLine("If the past is history, the future is mistery, today is a ...? That's why its called the present");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "gift")
+                {
+                    Console.WriteLine("Correct! The cages are now unlocked and the halflings are free. You received a reward that will help you in the forest, where your next quest awaits.");
+                    menu1 = false;
+                    victory4 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                }
+            }
         }
-        else if (questID == 5)
+        else if (questID == 5 && victory4 == true)
         {
             Console.WriteLine("Kill Aragog quest accepted");
+            Console.WriteLine(mission.Name);
+            Console.WriteLine(mission.Description);
+            if (monster.ID == 5)
+            {
+                //Introduce quest method logic
+                if (monster.CurrentHitPoints == 0)
+                {
+                    Console.WriteLine($"You have killed {monster.Name}. The villagers now live in piece and harmony, maybe its time to go back to bed.");
+                    victory5 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Keep trying!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect monster for this zone");
+            }
         }
         else
         {
-            Console.WriteLine("No quest available");
+            Console.WriteLine("You must complete another quest to start this quest");
         }
     }
+
+    // Generates a random number, if above 3, returns true, otherwise returns false
     public static bool CancelQuest()
     {
         int rolldice = new Random().Next(1, 7);
@@ -65,4 +259,9 @@ public class Quest
             return false; // The player suffers consequences, the quest fails to cancel
         }
     }
+    // public static void Main()
+    // {
+    //     EnterLocationWithAQuest(3);
+    //     EnterLocationWithAQuest(5);
+    // }
 }

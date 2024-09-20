243cf06bd3bbcb8f59544a9913d087db608dff63
rowin04 <116190178+rowin04@users.noreply.github.com>
2024-09-20 12:57:57 +0200

    fully functioning trade shop bug free hopefully🙏🏽

--- TradeShop.cs
+++ TradeShop.cs
@@ -6,7 +6,7 @@
 
 using System.Security.Cryptography.X509Certificates;
 
-static class TradeShop
+static class TradeShop //oli
 {
     public static bool running_shop = true;
     public static bool Selling = false;
@@ -46,6 +46,10 @@ static class TradeShop
                         else
                         {
                             Console.WriteLine("invalid input");
+                            Console.WriteLine("--------------------------------------");
+                            Console.WriteLine("[Enter]");
+                            Console.ReadLine();
+                            Console.Clear();
                         }
 
                     }
@@ -71,6 +75,10 @@ static class TradeShop
                         else
                         {
                             Console.WriteLine("invalid input");
+                            Console.WriteLine("--------------------------------------");
+                            Console.WriteLine("[Enter]");
+                            Console.ReadLine();
+                            Console.Clear();
                         }
                     }
 
@@ -82,7 +90,11 @@ static class TradeShop
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input"); // choice buy or sell;
+                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("--------------------------------------");
+                    Console.WriteLine("[Enter]");
+                    Console.ReadLine();
+                    Console.Clear(); // choice buy or sell;
                 }
             }
         }
@@ -106,7 +118,7 @@ static class TradeShop
             Console.WriteLine("--------------------------------------");
             Console.WriteLine("What item would you like to sell for currency. Give a number. (id)");
             string id = Console.ReadLine();
-            if (int.TryParse(id, out int trade_id))
+            if (int.TryParse(id, out int trade_id)) //so int is made but player doens't get error when entering id with something other than int
             {
                 foreach (var weapon in player.PlayerInventory.WeaponInventory)
                 {
@@ -117,13 +129,13 @@ static class TradeShop
                         while (confirmation_sell)
                         {
                             Console.WriteLine("--------------------------------------");
-                            Console.WriteLine($"Do you want to sell {weapon.Name} for {weapon.Price}: (Y/N) ");
+                            Console.WriteLine($"Do you want to sell {weapon.Name} for ${weapon.Price}: (Y/N) ");
                             Console.WriteLine("--------------------------------------");
                             string yes_no = Console.ReadLine().ToUpper();
                             if (yes_no == "Y")
                             {
-                                player.Coins += weapon.Price;
-                                player.RemoveItemFromInventory(null, World.WeaponByID(weapon.ID));
+                                player.Coins += weapon.Price; // money added to coins when item is sold
+                                player.RemoveItemFromInventory(null, World.WeaponByID(weapon.ID)); //removes sold item from player inventory
                                 Console.WriteLine($"You have removed {weapon.Name} from your inventory");
                                 Console.WriteLine("--------------------------------------");
                                 Console.WriteLine($"{weapon.Price} added to coins");
@@ -135,11 +147,11 @@ static class TradeShop
                                 Console.Clear();
                                 confirmation_sell = false;
                                 Selling = false;
-                                return player;
+                                return player; //return still bit of a mystery why it works lol
 
 
                             }
-                            else if (yes_no == "N")
+                            else if (yes_no == "N") //when no is entered get redirectd back to TradeshopOG
                             {
                                 Console.WriteLine("[Enter]");
                                 Console.ReadLine();
@@ -150,7 +162,11 @@ static class TradeShop
                             }
                             else
                             {
-                                Console.WriteLine("input error");
+                                Console.WriteLine("Invalid input"); //when invalid input is entered get redirectd back to TradeshopOG 
+                                Console.WriteLine("--------------------------------------");
+                                Console.WriteLine("[Enter]");
+                                Console.ReadLine();
+                                Console.Clear();
                                 confirmation_sell = false;
                                 Selling = false;
                                 return player;
@@ -160,7 +176,8 @@ static class TradeShop
                     }
                     else
                     {
-                        Console.WriteLine("You dont have this weapon.");
+                        Console.WriteLine("You dont have this weapon."); //when invalid input is entered get redirectd back to TradeshopOG
+                        Console.WriteLine("--------------------------------------");
                         Console.WriteLine("[Enter]");
                         Console.ReadLine();
                         Console.Clear();
@@ -174,6 +191,7 @@ static class TradeShop
             else
             {
                 Console.WriteLine("invalid input");
+                Console.WriteLine("--------------------------------------");
                 Console.WriteLine("[Enter]");
                 Console.ReadLine();
                 Console.Clear();
@@ -217,7 +235,7 @@ static class TradeShop
                         while (confirmation_sell)
                         {
                             Console.WriteLine("--------------------------------------");
-                            Console.WriteLine($"Do you want to sell {consumable.Name} for {consumable.Price}: (Y/N) ");
+                            Console.WriteLine($"Do you want to sell {consumable.Name} for ${consumable.Price}: (Y/N) ");
                             Console.WriteLine("--------------------------------------");
                             string yes_no = Console.ReadLine().ToUpper();
                             if (yes_no == "Y")
@@ -242,6 +260,7 @@ static class TradeShop
                             else if (yes_no == "N")
                             {
                                 Console.WriteLine("[Enter]");
+                                Console.WriteLine("--------------------------------------");
                                 Console.ReadLine();
                                 Console.Clear();
                                 confirmation_sell = false;
@@ -251,6 +270,10 @@ static class TradeShop
                             else
                             {
                                 Console.WriteLine("input error");
+                                Console.WriteLine("--------------------------------------");
+                                Console.WriteLine("[Enter]");
+                                Console.ReadLine();
+                                Console.Clear();
                                 confirmation_sell = false;
                                 Selling = false;
                                 return player;
@@ -261,6 +284,7 @@ static class TradeShop
                     else
                     {
                         Console.WriteLine("You dont have this consumable.");
+                        Console.WriteLine("--------------------------------------");
                         Console.WriteLine("[Enter]");
                         Console.ReadLine();
                         Console.Clear();
@@ -274,6 +298,7 @@ static class TradeShop
             else
             {
                 Console.WriteLine("invalid input");
+                Console.WriteLine("--------------------------------------");
                 Console.WriteLine("[Enter]");
                 Console.ReadLine();
                 Console.Clear();
@@ -286,7 +311,92 @@ static class TradeShop
         }
         return player;
     }
-    public static void BuyWeapon(Player player)
+    public static void BuyConsumable(Player player) //makes buying possible consumables
+    {
+        bool Buying = true;
+        while (Buying)
+        {
+            Console.WriteLine("This is our assortiment at the moment: ");
+            foreach (var consumable in World.Consumables)
+            {
+                Console.WriteLine($"Consumable ID: {consumable.ID}");
+                Console.WriteLine($"Consumable Name: {consumable.Name}");
+                Console.WriteLine($"Consumable Strength: {consumable.Strength}");
+                Console.WriteLine($"Consumable Usage: {consumable.Usage}");
+                Console.WriteLine($"Consumable Price: ${consumable.Price}");
+
+                Console.WriteLine("..................................");
+            }
+
+            player.DisplayInventory();// can be annoying cause switch weapon will be asked often.
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Name:{player.Name}\nHealth: {player.Current_Health}\nMoney: ${player.Coins}");
+            Console.WriteLine("What item would you like to Buy for currency. Give a number. (id)");
+
+            string id = Console.ReadLine();
+            if (int.TryParse(id, out int buy_id)) // to catch error if player gives input of non int otherwise will trip out somewhere  // could maybe delete it but too lazy to check lol
+            {
+                foreach (var consumable in World.Consumables) // go trough consumable wordl list to find same consumable as given id
+                {
+                    if (buy_id == consumable.ID) //get right weapon
+                    {
+                        if (player.Coins >= consumable.Price) //see if player can afford weapon
+                        {
+                            if (!player.PlayerInventory.ConsumableInventory.Contains(consumable)) //to check if player doesnt own consumable
+                            {
+                                Consumable bought_Consumable = World.ConsumableByID(consumable.ID);
+                                player.AddItemToInventory(bought_Consumable, null);
+                                player.Coins -= consumable.Price;
+                                Console.WriteLine("--------------------------------------");
+                                Console.WriteLine($"You have bought: {bought_Consumable.Name}");
+                                Console.WriteLine("--------------------------------------");
+                                Console.WriteLine("[Enter]");
+
+                                Console.ReadLine();
+                                Console.Clear();
+                                Buying = false;
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("--------------------------------------");
+                                Console.WriteLine("You already own this Consumable."); // cant own same thing cause will get fucked up in the loop
+                                Console.WriteLine("--------------------------------------");
+                                Console.WriteLine("[Enter]");
+                                Console.ReadLine();
+                                Console.Clear();
+                                Buying = false;
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("--------------------------------------");
+                            Console.WriteLine("You don't have enough money for this Consumable"); //not enough currency for weapon
+                            Console.WriteLine("--------------------------------------");
+                            Console.WriteLine("[Enter]");
+                            Console.ReadLine();
+                            Console.Clear();
+                            Buying = false;
+                        }
+
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid input (non matching id given) ");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("[Enter]");
+                Console.ReadLine();
+                Console.Clear();
+                Buying = false; // tryparse; 64
+            }
+
+        }
+    }
+    public static void BuyWeapon(Player player) // identical to BuyConsumable but with weapons
     {
         bool Buying = true;
         while (Buying)
@@ -325,7 +435,6 @@ static class TradeShop
                                 Console.WriteLine($"You have bought: {bought_weapon.Name}");
                                 Console.WriteLine("--------------------------------------");
                                 Console.WriteLine("[Enter]");
-
                                 Console.ReadLine();
                                 Console.Clear();
                                 Buying = false;
@@ -360,96 +469,15 @@ static class TradeShop
             }
             else
             {
-                Console.WriteLine("invalid input not int of any ");
+                Console.WriteLine("invalid input (non matching id given) 2 ");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("[Enter]");
+                Console.ReadLine();
+                Console.Clear();
                 Buying = false; // tryparse; 64
             }
 
         }
     }
-    public static void BuyConsumable(Player player)
-    {
-        bool Buying = true;
-        while (Buying)
-        {
-            Console.WriteLine("This is our assortiment at the moment: ");
-            foreach (var consumable in World.Consumables)
-            {
-                Console.WriteLine($"Consumable ID: {consumable.ID}");
-                Console.WriteLine($"Consumable Name: {consumable.Name}");
-                Console.WriteLine($"Consumable Strength: {consumable.Strength}");
-                Console.WriteLine($"Consumable Usage: {consumable.Usage}");
-                Console.WriteLine($"Consumable Price: ${consumable.Price}");
 
-                Console.WriteLine("..................................");
-            }
-
-            player.DisplayInventory();
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine($"Name:{player.Name}\nHealth: {player.Current_Health}\nMoney: ${player.Coins}");
-            Console.WriteLine("What item would you like to Buy for currency. Give a number. (id)");
-
-            string id = Console.ReadLine();
-            if (int.TryParse(id, out int buy_id))
-            {
-                foreach (var consumable in World.Consumables)
-                {
-                    if (buy_id == consumable.ID) //get right weapon
-                    {
-                        if (player.Coins >= consumable.Price)
-                        {
-                            if (!player.PlayerInventory.ConsumableInventory.Contains(consumable))
-                            {
-                                Consumable bought_Consumable = World.ConsumableByID(consumable.ID);
-                                player.AddItemToInventory(bought_Consumable,null);
-                                player.Coins -= consumable.Price;
-                                Console.WriteLine("--------------------------------------");
-                                Console.WriteLine($"You have bought: {bought_Consumable.Name}");
-                                Console.WriteLine("--------------------------------------");
-                                Console.WriteLine("[Enter]");
-
-                                Console.ReadLine();
-                                Console.Clear();
-                                Buying = false;
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("--------------------------------------");
-                                Console.WriteLine("You already own this Consumable.");
-                                Console.WriteLine("--------------------------------------");
-                                Console.WriteLine("[Enter]");
-                                Console.ReadLine();
-                                Console.Clear();
-                                Buying = false;
-                            }
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("--------------------------------------");
-                            Console.WriteLine("You don't have enough money for this Consumable");
-                            Console.WriteLine("--------------------------------------");
-                            Console.WriteLine("[Enter]");
-                            Console.ReadLine();
-                            Console.Clear();
-                            Buying = false;
-                        }
-
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("invalid input");
-                        Buying = false;
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine("invalid input not int of any ");
-                Buying = false; // tryparse; 64
-            }
-
-        }
-    }
 }
